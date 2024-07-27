a9a6e21d09c297813c18c79ce88c155ac2c1be9f
We Are Engineer <weerengineers@gmail.com>
2024-07-27 15:47:29 +0530

    minor update :)

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -2,13 +2,19 @@ let count = 0;
 let position = [];
 
 function addXO(e) {
-    console.log(count % 2)
-    if (count % 2 == 0) {
-        e.innerText = "X"
-        count += 1
-    } else {
-        e.innerText = "O"
-        count += 1
+
+    console.log(position.filter(p => p.position === '0')[0]?.value === position.filter(p => p.position === '1')[0]?.value && position.filter(p => p.position === '1')[0]?.value === position.filter(p => p.position === '2')[0]?.value)
+
+    if (position.filter(p => p.position == e.id).length === 0) {
+        if (count % 2 == 0) {
+            e.innerText = "X"
+            count += 1
+            position.push({ position: e.id, value: "X" })
+        } else {
+            e.innerText = "O"
+            count += 1
+            position.push({ position: e.id, value: "O" })
+        }
     }
-    console.log(e.id)
+    // console.log(e.id)
 }
