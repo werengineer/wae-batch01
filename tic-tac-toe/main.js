907361151a7f5a5c4d91d335544eca6e7255ec61
We Are Engineer <weerengineers@gmail.com>
2024-07-30 15:34:40 +0530

    completed tic tac toe app :)

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,11 +1,11 @@
 let count = 0;
 let position = [];
+const box = document.getElementsByClassName("box")
+let won = false;
 
 function addXO(e) {
 
-    console.log(position.filter(p => p.position === '0')[0]?.value === position.filter(p => p.position === '1')[0]?.value && position.filter(p => p.position === '1')[0]?.value === position.filter(p => p.position === '2')[0]?.value)
-
-    if (position.filter(p => p.position == e.id).length === 0) {
+    if (!won && position.filter(p => p.position == e.id).length === 0) {
         if (count % 2 == 0) {
             e.innerText = "X"
             count += 1
@@ -16,5 +16,42 @@ function addXO(e) {
             position.push({ position: e.id, value: "O" })
         }
     }
+    detectWin();
     // console.log(e.id)
+}
+
+let winState = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+function detectWin() {
+    
+    
+    for (let i = 0; i < winState.length; i++) {
+        const [a, b, c] = winState[i];
+        const boxA = box[a];
+        const boxB = box[b];
+        const boxC = box[c];
+
+        if (boxA.innerText && boxA.innerText === boxB.innerText && boxA.innerText === boxC.innerText) {
+            alert(boxA.innerText + " wins!");
+            won = true;
+            break;
+        }
+    }
+
+    if (!won && count === 9) {
+        alert("It's a draw!");
+    }
+}
+
+const resetGame = () =>{
+    window.location.reload()
 }
